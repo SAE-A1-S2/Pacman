325adb90da37c3c19a24340ca56974bbbfc61b70
cdricms <36056008+cdricms@users.noreply.github.com>
2024-05-03 18:02:48 +0200

    Executable proj

--- Engine/Behavior.cs
+++ Engine/Behavior.cs
@@ -1,27 +1,27 @@
 ﻿namespace Engine
 {
-    public interface IEnemyBehavior
-    {
-        void Move();
-    }
+  public interface IEnemyBehavior
+  {
+    void Move();
+  }
 
-    public class ChaserBehavior : IEnemyBehavior
-    {
-        public void Move() { }
-    }
+  public class ChaserBehavior : IEnemyBehavior
+  {
+    public void Move() { }
+  }
 
-    public class AmbusherBehavior : IEnemyBehavior
-    {
-        public void Move() { }
-    }
+  public class AmbusherBehavior : IEnemyBehavior
+  {
+    public void Move() { }
+  }
 
-    public class WhimsicalBehavior : IEnemyBehavior
-    {
-        public void Move() { }
-    }
+  public class WhimsicalBehavior : IEnemyBehavior
+  {
+    public void Move() { }
+  }
 
-    public class WandererBehavior : IEnemyBehavior
-    {
-        public void Move() { }
-    }
+  public class WandererBehavior : IEnemyBehavior
+  {
+    public void Move() { }
+  }
 }
